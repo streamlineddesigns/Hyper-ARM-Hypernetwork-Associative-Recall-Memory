85b9c41dcf8d8f8b5941077e9e85ef275889058c
streamlineddesigns <streamline.our.designs@gmail.com>
2026-08-18 17:14:14 -0400

    Multi-Hop & Directional Prototype Alignment & Sparse MoE

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,7 +9,7 @@ pip install tensorflow pandas numpy scikit-learn matplotlib
 #activate python venv
 source tf_env/bin/activate
 
-#[K Branch] train ae
+#[E Branch] train ae
 python train_ae_cnn.py
 
 #[V Branch] train ve mlp predictions
