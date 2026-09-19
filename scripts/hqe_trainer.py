b673ffb02e43c73bb2a0f996dc609fa1d0427360
streamlineddesigns <streamline.our.designs@gmail.com>
2026-09-18 23:43:46 -0400

    hyper param update

--- scripts/hqe_trainer.py
+++ scripts/hqe_trainer.py
@@ -147,7 +147,7 @@ HYBRID_USE_LTM_PROTO = True
 # Hypernetwork Config (From Script B)
 NUM_VISUAL_CENTROIDS = 128
 # *** NUM_CLASSES REMOVED - Calculated Dynamically from Data ***
-TARGET_NET_ARCH = [32, 32] #in 128-> 8, 8 -> out 128
+TARGET_NET_ARCH = [16, 16] #in 128-> 8, 8 -> out 128
 HYPER_INTERMEDIATE_DIM = 4 #latent dim to generate target network 32->
 
 # EDA Config (From Script B)
